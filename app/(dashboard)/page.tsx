936d9214e508208ff4ad9e08eccf7c76cccf4726
shahzaib834 <virgo834@gmail.com>
2024-02-21 15:22:10 +0500

    feat: added dashboard placeholder layout

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const DashboardPage = () => {
+  return <div>Dashboard Root page</div>;
+};
+
+export default DashboardPage;
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from '@/components/ui/button';
-import React from 'react';
-
-const Page = () => {
-  return <Button>Click me</Button>;
-};
-
-export default Page;
