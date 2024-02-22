85bd685e783456c5856c5fc22078f8781ccfd264
shahzaib834 <virgo834@gmail.com>
2024-02-22 11:40:15 +0500

    feat: main sidebar organizations listed

---
+++ app/(dashboard)/_components/sidebar/Item.tsx
@@ -0,0 +1,44 @@
+'use client';
+import { Hint } from '@/components/Hint';
+import { cn } from '@/lib/utils';
+import { useOrganization, useOrganizationList } from '@clerk/nextjs';
+import Image from 'next/image';
+import React from 'react';
+
+interface ItemProps {
+  id: string;
+  name: string;
+  imageUrl: string;
+}
+
+const Item = ({ id, name, imageUrl }: ItemProps) => {
+  const { organization } = useOrganization();
+  const { setActive } = useOrganizationList();
+
+  const isActive = organization?.id === id;
+
+  const onClick = () => {
+    if (!setActive) return;
+
+    setActive({ organization: id });
+  };
+
+  return (
+    <div className='aspect-square relative'>
+      <Hint label={name} side='right' align='start' sideOffset={18}>
+        <Image
+          src={imageUrl}
+          fill
+          alt={name}
+          onClick={onClick}
+          className={cn(
+            'rounded-md cursor-pointer opacity-75 hover:opacity-100 transition',
+            isActive && 'opacity-100'
+          )}
+        />
+      </Hint>
+    </div>
+  );
+};
+
+export default Item;
--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import NewButton from './NewButton';
+import List from './List';
 
 const Sidebar = () => {
   return (
     <aside className='fixed z-[1] left-0 w-[60px] h-full bg-blue-950 flex p-3 flex-col gap-y-4 text-white'>
-      Side
+      <List />
+      <NewButton />
     </aside>
   );
 };
