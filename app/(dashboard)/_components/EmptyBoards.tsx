4cf5aa57f24a845fa0ae446289ee3d516f1a3760
shahzaib834 <virgo834@gmail.com>
2024-02-22 13:48:49 +0500

    feat: convex create board mutation

--- app/(dashboard)/_components/EmptyBoards.tsx
+++ app/(dashboard)/_components/EmptyBoards.tsx
@@ -1,31 +1,31 @@
 'use client';
 
 import Image from 'next/image';
-//import { toast } from 'sonner';
+import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
 import { useOrganization } from '@clerk/nextjs';
 
 import { api } from '@/convex/_generated/api';
 import { Button } from '@/components/ui/button';
-//import { useApiMutation } from '@/hooks/use-api-mutation';
+import { useApiMutation } from '@/hooks/use-api-mutation';
 
 export const EmptyBoards = () => {
   const router = useRouter();
   const { organization } = useOrganization();
-  //const { mutate, pending } = useApiMutation(api.board.create);
+  const { mutate, pending } = useApiMutation(api.board.create);
 
   const onClick = () => {
     if (!organization) return;
 
-    // mutate({
-    //   orgId: organization.id,
-    //   title: 'Untitled',
-    // })
-    //   .then((id) => {
-    //     toast.success('Board created');
-    //     router.push(`/board/${id}`);
-    //   })
-    //   .catch(() => toast.error('Failed to create board'));
+    mutate({
+      orgId: organization.id,
+      title: 'Untitled',
+    })
+      .then((id) => {
+        toast.success('Board created');
+        //router.push(`/board/${id}`);
+      })
+      .catch(() => toast.error('Failed to create board'));
   };
 
   return (
@@ -36,7 +36,7 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className='mt-6'>
-        <Button onClick={onClick} size='lg'>
+        <Button onClick={onClick} disabled={pending} size='lg'>
           Create board
         </Button>
       </div>
