8df83f06e0ab12758d10c83f23bde64f86b80cca
shahzaib834 <virgo834@gmail.com>
2024-02-23 14:10:52 +0500

    feat: room participants shown

--- app/board/[boardId]/_components/Participants.tsx
+++ app/board/[boardId]/_components/Participants.tsx
@@ -1,10 +1,47 @@
-import { Skeleton } from '@/components/ui/skeleton';
+'use client';
+import { useOthers, useSelf } from '@/liveblocks.config';
 import React from 'react';
+import UserAvatar from './UserAvatar';
+import { connectionIdToColor } from '@/lib/utils';
+
+const MAX_SHOWN_OTHER_USERS = 2;
 
 const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_OTHER_USERS;
+
   return (
     <div className='absolute h-12 top-2 right-2 bg-white p-3 rounded-md flex items-center shadow-md'>
-      TODO:List of Users
+      <div className='flex gap-x-2'>
+        {users.slice(0, MAX_SHOWN_OTHER_USERS).map(({ connectionId, info }) => {
+          return (
+            <UserAvatar
+              borderColor={connectionIdToColor(connectionId)}
+              key={connectionId}
+              src={info?.picture}
+              name={info?.name}
+              fallback={info?.name?.[0] || 'T'}
+            />
+          );
+        })}
+
+        {currentUser && (
+          <UserAvatar
+            borderColor={connectionIdToColor(currentUser.connectionId)}
+            src={currentUser.info?.picture}
+            name={`${currentUser.info?.name} (You)`}
+            fallback={currentUser.info?.name?.[0] || 'Y'}
+          />
+        )}
+
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - MAX_SHOWN_OTHER_USERS} more`}
+            fallback={`+${users.length - MAX_SHOWN_OTHER_USERS}`}
+          />
+        )}
+      </div>
     </div>
   );
 };
